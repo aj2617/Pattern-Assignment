7bc24b702475345b00c8784f5e4be35e623d00c7
Aj shihab <149856067+aj2617@users.noreply.github.com>
2024-01-10 23:32:22 +0800

    Update pattern 31.c
    add another two approaches.

--- Pattern ASSIGNMENT/pattern 31.c
+++ Pattern ASSIGNMENT/pattern 31.c
@@ -1,20 +1,103 @@
+//way--1
 #include <stdio.h>
-
-int main() {
+int main()
+{
     int n;
     printf("Enter the number: ");
     scanf("%d", &n);
 
-    for (int i = 1; i <= 2 * n - 1; i++) {
-        for (int j = 1; j <= 2 * n - 1; j++) {
-            int min = (i < j) ? i : j;
-            min = (min < 2 * n - i) ? min : 2 * n - i;
-            min = (min < 2 * n - j + 1) ? min : 2 * n - j + 1;
+    int size = n*2 - 1;
 
-            printf("%d ", n - min + 1);
+    for(int i=0; i<size; i++)
+    {
+
+        for(int j=0; j<size; j++)
+        {
+
+            int min = i < j ? i : j;
+
+            min = min < size-i ? min : size-i-1; 
+
+            min = min < size-j-1 ? min : size-j-1; 
+
+            printf("%d ", n-min);
+
+        }
+
+        printf("\n");
+        
+    }
+
+}
+//way-2
+#include <stdio.h>
+
+void printPattern(int N) {
+    for (int i = 1; i <= 2 * N - 1; i++) {
+        int cnt = N;
+        for (int j = 1; j <= 2 * N - 1; j++) {
+            printf("%d ", cnt);
+
+            if (i > j)
+                cnt--;
+
+            if (i + j >= 2 * N) {
+                cnt++;
+            }
         }
         printf("\n");
     }
+}
+
+int main() {
+    int N;
+    printf("Enter the value of N: ");
+    scanf("%d", &N);
+
+    printPattern(N);
 
     return 0;
 }
+//way-3
+#include <stdio.h>
+
+void printPattern(int N) {
+    int totalMatrixSize = 2 * N - 1;
+    int start = 0;
+    int end = totalMatrixSize - 1;
+    int arr[totalMatrixSize][totalMatrixSize];
+
+    // Fill the 2D array
+    while (N != 0) {
+        for (int row = start; row <= end; row++) {
+            for (int col = start; col <= end; col++) {
+                if (row == start || row == end || col == start || col == end) {
+                    arr[row][col] = N;
+                }
+            }
+        }
+        start++;
+        end--;
+        N--;
+    }
+
+    // Print the 2D array
+    for (int row = 0; row < totalMatrixSize; row++) {
+        for (int col = 0; col < totalMatrixSize; col++) {
+            printf("%d ", arr[row][col]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int N;
+    printf("Enter the value of N: ");
+    scanf("%d", &N);
+
+    printPattern(N);
+
+    return 0;
+}
+//way-4
+
